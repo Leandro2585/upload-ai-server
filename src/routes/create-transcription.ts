45070f62c2debe5d6f3ecaef318a352fbd1ee4ba
leandro.real <leandro.real.vieira.timeware@outlook.com>
2023-09-13 09:12:00 -0300

    feat: add create transcription validations

---
+++ src/routes/create-transcription.ts
@@ -0,0 +1,20 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+
+export const createTranscriptionRoute = async (app: FastifyInstance) => {
+    app.post('/videos/:videoId/transcription', async (request) => {
+        
+        const paramsSchema = z.object({
+            videoId: z.string().uuid()
+        })
+        const { videoId } = paramsSchema.parse(request.params)
+        const bodySchema = z.object({
+            prompt: z.string()
+        })
+        const { prompt } = bodySchema.parse(request.body)
+        return {
+            videoId,
+            prompt
+        }
+    })
+}
