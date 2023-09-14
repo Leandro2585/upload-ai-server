03bb324b230716a2715858cba188239788909c77
leandro.real <leandro.real.vieira.timeware@outlook.com>
2023-09-13 21:55:40 -0300

    feat: add AI integration using OpenAI with GPT 3.5 Turbo

---
+++ src/routes/index.ts
@@ -0,0 +1,4 @@
+export * from "./generate-ai-completion"
+export * from "./create-transcription"
+export * from "./get-all-prompts"
+export * from "./upload-video"
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import { fastify } from 'fastify'
-import { prisma } from './lib/prisma'
-import { getAllPromptsRoute } from './routes/get-all-prompts'
-import { uploadVideoRoute } from './routes/upload-video'
-import { createTranscriptionRoute } from './routes/create-transcription'
+import { fastifyCors } from '@fastify/cors'
+import 'dotenv/config'
+import { createTranscriptionRoute, generateAICompletionRoute, getAllPromptsRoute, uploadVideoRoute } from './routes'
 
 const app = fastify()
+
+app.register(fastifyCors, {
+    origin: '*'
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
 
 app.listen({
     port: 3333
