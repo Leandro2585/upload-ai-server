9a42bfc318a41c44cb3e9d9a43a2168d0ad11979
leandro.real <leandro.real.vieira.timeware@outlook.com>
2023-09-17 10:06:56 -0300

    finish project

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
+import { OpenAIStream, streamToResponse } from "ai";
 import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
 
@@ -7,10 +8,10 @@ export const generateAICompletionRoute = async (app: FastifyInstance) => {
     app.post("/ai/complete", async (request, reply) => {
         const bodySchema = z.object({
             videoId: z.string().uuid(),
-            template: z.string(),
+            prompt: z.string(),
             temperature: z.number().min(0).max(1).default(0.5)
         });
-        const { videoId, temperature, template } = bodySchema.parse(request.body)
+        const { videoId, temperature, prompt } = bodySchema.parse(request.body)
         const video = await prisma.video.findUniqueOrThrow({
             where: {
                 id: videoId
@@ -20,15 +21,22 @@ export const generateAICompletionRoute = async (app: FastifyInstance) => {
             return reply.status(400).send({ error: 'Video transcription was not generated yet.'})
         }
 
-        const promptMsg = template.replace('{transcription}', video.transcription)
+        const promptMsg = prompt.replace('{transcription}', video.transcription)
 
         const response = await openai.chat.completions.create({
-            model: 'gpt-3.5-turbo-16k',
+            model: "gpt-3.5-turbo-16k",
             temperature,
-            messages: [
-                { role: 'user', content: promptMsg }
-            ]
-        })
-        return response
+            messages: [{ role: "user", content: promptMsg }],
+            stream: true,
+          });
+      
+          const stream = OpenAIStream(response);
+      
+          streamToResponse(stream, reply.raw, {
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            },
+          });
     });
 }
